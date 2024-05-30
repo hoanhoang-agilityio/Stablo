8cd957597af0b9a26205b6cfd108849f49af72dc
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2024-05-30 09:49:49 +0700

    Feat / Implement Footer component

---
+++ stablo/src/layouts/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => (
+  <footer className="border-t border-gray-300">
+    <div className="flex flex-col items-center p-8 mx-auto mt-10 max-w-2xl">
+      <p className="text-sm leading-5 text-center">
+        Copyright © 2024 Stablo. All rights reserved.
+      </p>
+      <p className="text-sm leading-5 text-gray-500 mt-1 text-center">
+        Made by Web3Templates
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
