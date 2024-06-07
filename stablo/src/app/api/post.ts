00076440bb7c1e73c070fb57bdc97d3ba472cf13
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2024-06-07 17:18:44 +0700

    Feat / Implement Route handler for Category API

--- stablo/src/app/api/post.ts
+++ stablo/src/app/api/post.ts
@@ -15,30 +15,30 @@
 //   return res.json();
 // };
 
-export const getPostListPagination = async (page?: number) => {
-  const url = page
-    ? `${process.env.API_END_POINT}/posts?_page=${page}&_limit=6`
-    : `${process.env.API_END_POINT}/posts`;
+// export const getPostListPagination = async (page?: number) => {
+//   const url = page
+//     ? `${process.env.API_END_POINT}/posts?_page=${page}&_limit=6`
+//     : `${process.env.API_END_POINT}/posts`;
 
-  const res = await fetch(url, {
-    cache: 'no-store',
-  });
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+//   const res = await fetch(url, {
+//     cache: 'no-store',
+//   });
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data');
+//   }
 
-  const postListResponse = await res.json();
-  const totalItem = res.headers.get('X-Total-Count');
+//   const postListResponse = await res.json();
+//   const totalItem = res.headers.get('X-Total-Count');
 
-  return {
-    post: postListResponse,
-    totalItem,
-  };
-};
+//   return {
+//     post: postListResponse,
+//     totalItem,
+//   };
+// };
 
 export const getPostListFiltered = async (authorId: string) => {
   const res = await fetch(
