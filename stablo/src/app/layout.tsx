610884a82a74b12df61ce9852633af7531f99f2b
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2024-09-06 17:09:01 +0700

    update test id

--- stablo/src/app/layout.tsx
+++ stablo/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           __html: `
           (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:5125421,hjsv:6};
+            h._hjSettings={hjid:5125538,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
