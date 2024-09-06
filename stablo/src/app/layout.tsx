2045c83e5248323d0fda6be046990dda91f33445
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2024-09-06 16:41:02 +0700

    test hotjar

--- stablo/src/app/layout.tsx
+++ stablo/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import Header from '@/layouts/Header';
 import Footer from '@/layouts/Footer';
 import './globals.css';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +24,20 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:5125421,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    `,
+        }}
+      />
     </html>
   );
 }
